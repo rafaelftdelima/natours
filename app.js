9409fcefe6297f450b61e9b6e475bf5fee11de35
Rafael de Lima <rafaelftdelima@gmail.com>
2023-01-16 22:00:04 -0300

    GET request exercise

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-const { request } = require('express');
+const fs = require('fs');
+const { request, response } = require('express');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+/* app.get('/', (request, response) => {
     response.status(200).json({ message: 'Hello from the server-side!', app: 'Natours' });
 });
 
 app.post('/', (request, response) => {
     response.send('You can post to the endpoint...');
+}); */
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (request, response) => {
+    response.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours,
+        },
+    });
 });
 
 const port = 1111;

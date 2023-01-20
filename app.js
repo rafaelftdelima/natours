aa4d9805a809371ec05198df6c239766434fb9a6
Rafael de Lima <rafaelftdelima@gmail.com>
2023-01-19 22:00:30 -0300

    Responding to URL parameters

--- app.js
+++ app.js
@@ -16,12 +16,24 @@ app.post('/', (request, response) => {
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
-app.get('/api/v1/tours', (request, response) => {
+app.get('/api/v1/tours/:id', (request, response) => {
+    console.log(request.params);
+
+    const id = request.params.id * 1;
+    const tour = tours.find((element) => element.id === id);
+
+    if (!tour) {
+        return response.status(404).json({
+            status: 'fail',
+            message: 'invalid id',
+        });
+    }
+
     response.status(200).json({
         status: 'success',
         results: tours.length,
         data: {
-            tours,
+            tour,
         },
     });
 });

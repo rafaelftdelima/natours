220bd4a1b1a273c625bfae739b1a99ba9197e21d
Rafael de Lima <rafaelftdelima@edu.unirio.br>
2023-03-09 23:10:05 -0300

    Code correction for last commit

--- routers/tourRouter.js
+++ routers/tourRouter.js
@@ -4,7 +4,7 @@ const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
 
-router.param('id', tourController.checkID₢);
+router.param('id', tourController.checkID);
 
 router.route('/').get(tourController.getAllTours).post(tourController.createTour);
 

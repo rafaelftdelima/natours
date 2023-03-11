251f004850c578dfef64267c459def1173e358f1
Rafael de Lima <rafaelftdelima@edu.unirio.br>
2023-03-11 08:23:41 -0300

    Add mongodb connection to database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({
@@ -6,7 +7,39 @@ dotenv.config({
 
 const app = require('./app');
 
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>', process.env.DATABASE_PASSWORD
+);
+
+/* DATABASE LOCAL */
+/* mongoose.connect(process.env.DATABASE_LOCAL).then(
+    () => console.log('DB connection successful')
+); */
+
+/* DATABASE FROM ATLAS */
+mongoose.connect(DB).then(() => {
+    console.log('DB connection successful');
+});
+
 /* console.log(process.env); */
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price']
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 1111;
 
